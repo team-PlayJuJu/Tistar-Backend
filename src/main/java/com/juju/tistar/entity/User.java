c8041d9b423c1a797c41e0f39ca80a07e2270f66
2rjswn <h47841930@gmail.com>
2024-05-20 00:07:01 +0900

    � :: [#1] Add Entity

--- src/main/java/com/juju/tistar/entity/User.java
+++ src/main/java/com/juju/tistar/entity/User.java
@@ -1,9 +1,6 @@
 package com.juju.tistar.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import javax.annotation.processing.Generated;
 
@@ -12,6 +9,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
+    @Column
+    private String Image;
 }
