ff64b80d9ced659d19d519fbeda0f11b88eaf897
2rjswn <h47841930@gmail.com>
2024-05-26 23:54:25 +0900

    [#11] Create-Exception

--- src/main/java/com/juju/tistar/controller/UserController.java
+++ src/main/java/com/juju/tistar/controller/UserController.java
@@ -1,19 +1,21 @@
 package com.juju.tistar.controller;
 
 import com.juju.tistar.entity.User;
+import com.juju.tistar.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signUp(@RequestBody User user) {
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body("회원가입 성공");
+    public ResponseEntity<User> signUp(@RequestBody User user) {
+        return ResponseEntity.ok(userService.signupUser(user));
     }
 
     @GetMapping("/signin")
--- src/main/java/com/juju/tistar/service/UserService.java
+++ src/main/java/com/juju/tistar/service/UserService.java
@@ -1,22 +1,39 @@
 package com.juju.tistar.service;
 
+import com.juju.tistar.entity.Authority;
 import com.juju.tistar.entity.User;
+import com.juju.tistar.repository.AuthorityRepository;
 import com.juju.tistar.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final AuthorityRepository authorityRepository;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @Transactional
     public User signupUser(User user) {
+        if (user.getName() == null || user.getName().isEmpty() || user.getPwd() == null || user.getPwd().isEmpty()) {
+            throw new IllegalArgumentException("이름 또는 비번이 없음");
+        }
         String hashedPwd = passwordEncoder.encode(user.getPwd());
         user.setPwd(hashedPwd);
         user.setCreateDt(String.valueOf(new Date(System.currentTimeMillis())));
-        return userRepository.save(user);
+        userRepository.save(user);
+        Authority authority = new Authority();
+        authority.setName("ROLE_USER");
+        authority.setUser(user);
+        authorityRepository.save(authority);
+        return user;
     }
+
+
+
 }
